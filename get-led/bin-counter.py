07f5bd5788a22cb28d8e1f4ab1be33dd71153ff5
Naidysh Maria <naidysh.ms@phystech.edu>
2026-09-17 18:30:16 +0300

    теперь работает

--- get-led/bin-counter.py
+++ get-led/bin-counter.py
@@ -6,6 +6,7 @@ GPIO.setup(leds, GPIO.OUT)
 GPIO.output(leds, 0)
 up = 9
 down = 10
+GPIO.setup([up, down], GPIO.IN)
 num = 0
 def dec2bin(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
@@ -15,7 +16,7 @@ while True:
         num = num + 1
         print(num, dec2bin(num))
         time.sleep(sleep_time)
-    if not GPIO.input(down):
+    if GPIO.input(down):
         num = num - 1
         if num < 0:
             num = 0
